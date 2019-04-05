4f3b6ce2d5ac438689b746190221ac2c051775be
Starfall Production <starfallproduction42@gmail.com>
2019-04-05 21:04:41 +0700

    - Migrate to AndroidX - Remove all boilerplate codes - Change in gradle file

--- app/src/main/java/id/ac/umn/mykos/MainActivity.java
+++ app/src/main/java/id/ac/umn/mykos/MainActivity.java
@@ -1,10 +1,9 @@
 package id.ac.umn.mykos;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
