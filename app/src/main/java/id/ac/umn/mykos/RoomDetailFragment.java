08b1518a9277df838a16fe83b81a9cfb0ca76b84
Budiman Putra Jaya <42717955+lieahau@users.noreply.github.com>
2019-04-11 01:53:37 +0700

    Added action menu in room detail toolbar

--- app/src/main/java/id/ac/umn/mykos/RoomDetailFragment.java
+++ app/src/main/java/id/ac/umn/mykos/RoomDetailFragment.java
@@ -1,12 +1,18 @@
 package id.ac.umn.mykos;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.ActionMenuView;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -16,9 +22,48 @@ public class RoomDetailFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_room_detail, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setHasOptionsMenu(true);
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+        ((MainActivity)getActivity()).setActionBar(toolbar);
+        ((MainActivity)getActivity()).setHomeButton(R.drawable.ic_arrow_back);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                getActivity().onBackPressed();
+                return true;
+            case R.id.btn_reset:
+                /* TODO: RESET HERE */
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.roomdetail_menu, menu);
+        changeActionMenuItemsBackground(getResources().getColor(R.color.colorRed));
+    }
+
+    public void changeActionMenuItemsBackground(int color){
+        Toolbar toolbar = getActivity().findViewById(R.id.toolbar);
+        for (int i = 0; i < toolbar.getChildCount(); i++) {
+            final View v = toolbar.getChildAt(i);
+            if (v instanceof ActionMenuView) {
+                v.setBackgroundColor(color);
+            }
+        }
     }
 }
